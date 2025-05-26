609af909f1c9b2b8fba90ab0ca33678b63ff2ac8
Hussain Nawaz <husanawaz@gmail.com>
2025-05-26 22:44:56 +0500

    full backend

--- server.js
+++ server.js
@@ -9,11 +9,10 @@ const PORT = 1000;
 app.use(cors());
 require('dotenv').config();
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 //authentication
